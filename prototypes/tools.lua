0a9351949749f88f6dd260c9fe1e69e05874e53b
Robonon <robin.s.ahlqvist@gmail.com>
2026-02-02 21:15:27 +0100

    fix: remove old command tool

--- prototypes/tools.lua
+++ prototypes/tools.lua
@@ -3,20 +3,5 @@
 
 local tools = {}
 
--- Selection tool for deploying battalions (ephemeral - cursor only)
-tools.selection_tools = {
-  {
-    type = "selection-tool",
-    name = "command-order-tool",
-    icon = "__base__/graphics/icons/arrows/down-arrow.png",
-    alt_select = { mode = "nothing", border_color = {r=0, g=1, b=0}, cursor_box_type = "entity" },
-    stack_size = 1,
-    flags = {},
-    subgroup = "tool",
-    order = "c[automated-construction]-a[blueprint]",
-    select_sound = { filename = "__core__/sound/selection-tool-select.ogg" },
-    select = { mode = "nothing", border_color = {r=0, g=1, b=0}, cursor_box_type = "entity" }
-  }
-}
 
 return tools
