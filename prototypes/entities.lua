004ece270f06d6d3450326c7f7495a6a871cd313
Robonon <robin.s.ahlqvist@gmail.com>
2026-02-05 00:40:10 +0100

    fix: soldier animations

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -9,12 +9,10 @@ local character = data.raw["character"]["character"]
 -- UNITS
 -- ============================================
 
--- Regular soldier unit
 entities.soldier = {
   type = "unit",
   name = "soldier-unit",
   icon = "__base__/graphics/icons/submachine-gun.png",
-  icon_size = 64,
   flags = {"placeable-player", "placeable-off-grid", "breaths-air", "get-by-unit-number"},
   max_health = 150,
   healing_per_tick = 0.01,
@@ -22,8 +20,8 @@ entities.soldier = {
   selection_box = {{-0.4, -0.7}, {0.4, 0.4}},
   movement_speed = 0.15,
   distance_per_frame = 0.13,
-  run_animation = character.animations[1].running_with_gun,
-  idle_animation = character.animations[1].idle_with_gun,
+  run_animation = character.animations[1].running,
+  idle_animation = character.animations[1].idle_with_gun_in_air,
   vision_distance = 30,
   has_belt_immunity = true,
   can_open_gates = true,  -- Allows soldiers to trigger gates
@@ -52,51 +50,7 @@ entities.soldier = {
         }
       }
     },
-    animation = character.animations[1].idle_with_gun
-  },
-  distraction_cooldown = 0,
-  dying_explosion = "blood-explosion-small",
-  corpse = "character-corpse"
-}
-
--- HQ Squad unit (used for deploying new HQs)
-entities.hq_squad = {
-  type = "unit",
-  name = "hq-squad-unit",
-  icon = "__base__/graphics/icons/repair-pack.png",
-  icon_size = 64,
-  flags = {"placeable-player", "placeable-off-grid", "breaths-air"},
-  max_health = 300,
-  healing_per_tick = 0.01,
-  collision_box = {{-0.2, -0.2}, {0.2, 0.2}},
-  selection_box = {{-0.5, -0.8}, {0.5, 0.5}},
-  movement_speed = 0.12,  -- Slightly slower (carrying equipment)
-  distance_per_frame = 0.10,
-  run_animation = character.animations[1].running_with_gun,
-  idle_animation = character.animations[1].idle_with_gun,
-  vision_distance = 30,
-  has_belt_immunity = true,
-  ai_settings = {
-    destroy_when_commands_fail = false,
-    allow_try_return_to_spawner = false,
-    do_separation = true,
-  },
-  attack_parameters = {
-    type = "projectile",
-    ammo_category = "bullet",
-    range = 12,
-    cooldown = 30,
-    ammo_type = {
-      category = "bullet",
-      action = {
-        type = "direct",
-        action_delivery = {
-          type = "instant",
-          target_effects = {{type = "damage", damage = {amount = 3, type = "physical"}}}
-        }
-      }
-    },
-    animation = character.animations[1].idle_with_gun
+    animation = character.animations[1].idle_with_gun,
   },
   distraction_cooldown = 0,
   dying_explosion = "blood-explosion-small",
@@ -106,6 +60,75 @@ entities.hq_squad = {
 -- ============================================
 -- BUILDINGS
 -- ============================================
+
+entities.brigade_hq = {
+  type = "container",
+  name = "brigade-hq",
+  icon = "__base__/graphics/icons/radar.png",
+  icon_size = 64,
+  flags = {"player-creation"},
+  minable = {mining_time = 1.0, result = "brigade-hq"},
+  max_health = 1000,
+  corpse = "big-remnants",
+  collision_box = {{-1.9, -1.9}, {1.9, 1.9}},
+  selection_box = {{-2.0, -2.0}, {2.0, 2.0}},
+  inventory_size = 40,
+  picture = {
+    layers = {{
+      filename = "__base__/graphics/entity/radar/radar.png",
+      width = 196,
+      height = 254,
+      shift = {0.7, -0.75},
+      scale = 0.8
+    }}
+  },
+}
+
+entities.battalion_hq = {
+  type = "container",
+  name = "battalion-hq",
+  icon = "__base__/graphics/icons/radar.png",
+  icon_size = 64,
+  flags = {"player-creation"},
+  minable = {mining_time = 0.5, result = "battalion-hq"},
+  max_health = 500,
+  corpse = "medium-remnants",
+  collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
+  selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+  inventory_size = 20,
+  picture = {
+    layers = {{
+      filename = "__base__/graphics/entity/radar/radar.png",
+      width = 196,
+      height = 254,
+      shift = {0.7, -0.75},
+      scale = 0.5
+    }}
+  },
+}
+
+entities.company_hq = {
+  type = "container",
+  name = "company-hq",
+  icon = "__base__/graphics/icons/radar.png",
+  icon_size = 64,
+  flags = {"placeable-neutral", "player-creation"},
+  minable = {mining_time = 0.5, result = "company-hq"},
+  max_health = 400,
+  corpse = "big-remnants",
+  collision_box = {{-1.6, -1.6}, {1.6, 1.6}},
+  selection_box = {{-1.7, -1.7}, {1.7, 1.7}},
+  inventory_size = 20,
+  picture = {
+    layers = {{
+      filename = "__base__/graphics/entity/radar/radar.png",
+      width = 196,
+      height = 254,
+      shift = {0.7, -0.75},
+      scale = 0.6
+    }}
+  },
+}
 
 entities.platoon_hq = {
   type = "container",
@@ -133,54 +156,6 @@ entities.platoon_hq = {
       height = 254,
       shift = {0.7, -0.75},
       scale = 0.4
-    }}
-  },
-}
-
--- Brigade HQ (top-level, spawns HQ squads to deploy Battalion HQs)
-entities.brigade_hq = {
-  type = "container",
-  name = "brigade-hq",
-  icon = "__base__/graphics/icons/radar.png",
-  icon_size = 64,
-  flags = {"player-creation"},
-  minable = {mining_time = 1.0, result = "brigade-hq"},
-  max_health = 1000,
-  corpse = "big-remnants",
-  collision_box = {{-1.9, -1.9}, {1.9, 1.9}},
-  selection_box = {{-2.0, -2.0}, {2.0, 2.0}},
-  inventory_size = 40,
-  picture = {
-    layers = {{
-      filename = "__base__/graphics/entity/radar/radar.png",
-      width = 196,
-      height = 254,
-      shift = {0.7, -0.75},
-      scale = 0.8
-    }}
-  },
-}
-
--- Battalion HQ (mid-level, spawns squads)
-entities.battalion_hq = {
-  type = "container",
-  name = "battalion-hq",
-  icon = "__base__/graphics/icons/radar.png",
-  icon_size = 64,
-  flags = {"player-creation"},
-  minable = {mining_time = 0.5, result = "battalion-hq"},
-  max_health = 500,
-  corpse = "medium-remnants",
-  collision_box = {{-1.4, -1.4}, {1.4, 1.4}},
-  selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-  inventory_size = 20,
-  picture = {
-    layers = {{
-      filename = "__base__/graphics/entity/radar/radar.png",
-      width = 196,
-      height = 254,
-      shift = {0.7, -0.75},
-      scale = 0.5
     }}
   },
 }
